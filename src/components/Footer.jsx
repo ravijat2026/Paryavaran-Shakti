b4e0cf96d291881a7a76fb393b199e4ebff846c1
yashjangir04 <jangiryash04@gmail.com>
2024-11-04 18:08:07 +0530

    Modified

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
-  return (
-    <div className='min-h-screen flex justify-center items-center'>Footer</div>
-  )
-}
+  const currentYear = new Date().getFullYear();
 
-export default Footer
+  return (
+    <div className="min-w-full min-h-[50vh] bg-gradient-to-tl from-[#6bd2d3] to-[#91daea] flex justify-center items-center">
+      Footer
+    </div>
+  );
+};
+
+export default Footer;
